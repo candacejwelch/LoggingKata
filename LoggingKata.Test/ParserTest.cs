4746e1049ce9f6e3472ac96efd458501c6ec7c53
Candace <candacejwelch@gmail.com>
2017-10-31 13:15:38 -0500

    Completed test

--- LoggingKata.Test/ParserTest.cs
+++ LoggingKata.Test/ParserTest.cs
@@ -8,13 +8,40 @@ namespace LoggingKata.Test
         [Test]
         public void ShouldParseLine()
         {
-            //TODO: Complete ShouldParseLine
             //arrange
+            var tacoParser = new TacoParser();
+
+            var lessThanThree = "12334, 12333";
+            var longNotNumber = "abc, 1234, Testing";
+            var latNotNumber = "abc, 123, Testing";
 
             //act
+            var lessThanThreeValue = tacoParser.ParseTacos(lessThanThree);
+            var longNotNumberValue = tacoParser.ParseTacos(longNotNumber);
+            var latNotNumberValue = tacoParser.ParseTacos(latNotNumber);
+
 
             //asert
-            
+            Assert.Null(lessThanThreeValue, "lest than 3 should not parse");
+            Assert.Null(longNotNumberValue, "long not number should not parse");
+            Assert.Null(latNotNumberValue, "lat not number should not parse");
+
+
+        }
+
+        [Test]
+        public void ShouldNotParseLine()
+        {
+            //arrange
+            var tacoParser = new TacoParser();
+            var correctString = "123.04, 456.07, TestingName";
+
+            //act
+            var correctStringValue = tacoParser.ParseTacos(correctString);
+
+            //asert
+            Assert.NotNull(correctStringValue, "Should pass");
+
 
         }
     }
